f6411f88d1cbf921f599bfc50a6f7bbb3158b137
이희재 <>
2025-02-13 15:50:45 +0900

    Modify 넘파이 정리

--- numpy_practice.py
+++ numpy_practice.py
@@ -1,20 +1,25 @@
 import numpy as np
 
-data = [1, 2, 3, 4] # type = numpy.ndarray
-arr = np.array(data) * 10
+data = [1, 2, 3, 4] # type = list
+np_data = np.array(data) # type = numpy.ndarray
 
-# print(type(arr), arr)
+print(data)
+print(np_data)
 
-data1 = np.array([
+list_arr = data * 10
+np_arr = np.array(data) * 10
+
+print(list_arr)
+print(np_arr)
+
+multi_arr = np.array([
     [1, 2, 3, 4],
     [10, 20, 30, 40],
     [100, 200, 300, 400]
 ])
 
-# 행 단위로 인덱싱
 # 행 > 열 순서로 접근
-
-print(data1[0]) # [1 2 3 4]
-print(data1[0][1]) # 2
-print(data1[0, 1]) # 2
-print(data1[:, 0]) # [1 10 100]
+print(multi_arr[0]) # [1 2 3 4]
+print(multi_arr[1][2]) # 30
+print(multi_arr[1, 2]) # 30
+print(multi_arr[:, 3]) # [4 40 400]
