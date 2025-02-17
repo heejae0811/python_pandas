60209681931443f05cfd8dbe3ce76103e0683eef
이희재 <>
2025-02-17 14:42:46 +0900

    Modify 시리즈 생성, 인덱싱, 슬라이싱

--- series_practice01.py
+++ series_practice01.py
@@ -1,23 +1,17 @@
-import pandas as pd
 from pandas import Series
+import pandas as pd
 
-data = [0, 1, 2, 3] # type = list
-
-p = pd.Series(data) # type = pandas.core.series.Series
-
-print(type(data), type(p))
-print(p)
+data = ['가', '나', '다', '라'] # type = list
+s = pd.Series(data) # type = pandas.core.series.Series
+print(s)
 
 data = [100, 200, 300]
 index = ["월", "화", "수"]
 s = Series(data, index)
 print(s)
-
-print("1", s.index)
-print("1", s.values)
-print("1", s.array)
+print(s.index, s.values, s.array)
 
 name = ["메로나", "누가바", "빠삐코"]
 price = [500, 800, 200]
-menu = Series(name, price) # Series(data, index)
-print(type(menu), menu)
+menu = Series(name, price)
+print(menu)
--- series_practice02.py
+++ series_practice02.py
@@ -3,18 +3,25 @@ import pandas as pd
 data = [100, 200, 300]
 index = ["월", "화", "수"]
 s = pd.Series(data, index)
-print(s)
 
-print(s.iloc[0])
-print(s.iloc[1])
-print(s.iloc[2])
-print(s.iloc[-1])
+print(s.iloc[0]) # 100
+print(s.iloc[1]) # 200
+print(s.iloc[2]) # 300
+print(s.iloc[3]) # IndexError
 
-print(s.loc["월"])
-print(s.loc["화"])
-print(s.loc["수"])
+print(s.iloc[-1]) # 300
+print(s.iloc[-2]) # 200
+print(s.iloc[-3]) # 200
+print(s.iloc[-4]) # IndexError
 
-print(s.iloc[0:2])
-print(s.loc["월":"화"])
+print(s.loc["월"]) # 100
+print(s.loc["화"]) # 200
+print(s.loc["수"]) # 300
 
-print(s.iloc[[0, 2]])
+# 인덱싱 (개별 값)
+print(s.iloc[[0, 2]]) # 월 100 수 300
+print(s.loc[["월", "수"]]) # 월 100 수 300
+
+# 슬라이싱 (범위)
+print(s.iloc[0:2]) # 월 100 화 200
+print(s.loc["월":"화"]) # 월 100 화 200
